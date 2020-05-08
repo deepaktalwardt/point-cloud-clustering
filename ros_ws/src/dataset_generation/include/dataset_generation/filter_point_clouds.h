f707e4f452fcb33c15a12df17389ef786611f453
Parshwa27 <parshwa.gandhi27@gmail.com>
2020-05-07 22:28:29 -0700

    Added visualization functionality

--- ros_ws/src/dataset_generation/include/dataset_generation/filter_point_clouds.h
+++ ros_ws/src/dataset_generation/include/dataset_generation/filter_point_clouds.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <dirent.h>
 #include <iostream>
 #include <thread>
 #include <string>
 #include <sstream>
-#include <unordered_set>
 
 #include <pcl/ModelCoefficients.h>
 #include <pcl/PCLPointCloud2.h>
@@ -14,13 +12,12 @@
 #include <pcl/point_types.h>
 #include <pcl/common/common_headers.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
-#include "extract_point_cloud_objects.h"
 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-#include "dataset_generation/json.hpp"
 
 using namespace std::literals::chrono_literals;
 
@@ -28,47 +25,81 @@ namespace dataset_generation
 {
 
 
-class PCDFiltering
+class PointCloudFiltering
 {
 public:
-    PCDFiltering(
+    PointCloudFiltering(
         std::string in_folder_path,
-        std::string object_name,
         std::string out_folder_path);
 
-    ~PCDFiltering();
+    ~PointCloudFiltering();
 
-    void VoxelFilter(float leaf_size_x, float leaf_size_y, float leaf_size_z);
+    void apply_voxel_filter_and_save(float leaf_size_x, float leaf_size_y, float leaf_size_z);
+    void apply_voxel_filter_and_visualize(float leaf_size_x, float leaf_size_y, float leaf_size_z);
 private:
     std::string in_folder_path_;
-    std::string object_name_;
     std::string out_folder_path_;
 };
-PCDFiltering::PCDFiltering(
+PointCloudFiltering::PointCloudFiltering(
         std::string in_folder_path,
-        std::string object_name,
         std::string out_folder_path)
 {
     in_folder_path_=in_folder_path;
-    object_name_=object_name;
     out_folder_path_=out_folder_path;
 }
-PCDFiltering::~PCDFiltering()
+PointCloudFiltering::~PointCloudFiltering()
 {}
 
 /*
 This method is a modified version of the VoxelGrid filtering found at: 
 https://pcl-tutorials.readthedocs.io/en/master/voxel_grid.html?highlight=voxelGrid
 */
-void PCDFiltering::VoxelFilter(float leaf_size_x, float leaf_size_y, float leaf_size_z)
+void PointCloudFiltering::apply_voxel_filter_and_visualize(float leaf_size_x, float leaf_size_y, float leaf_size_z)
 {
     pcl::PCLPointCloud2::Ptr cloud_read (new pcl::PCLPointCloud2 ());
     pcl::PCLPointCloud2::Ptr cloud_voxelized (new pcl::PCLPointCloud2 ());
     
     //Read PCD files from the path given
-    std::string path=in_folder_path_+object_name_+".pcd";
     pcl::PCDReader reader;
-    reader.read(path, *cloud_read);
+    reader.read(in_folder_path_, *cloud_read);
+
+    pcl::VoxelGrid<pcl::PCLPointCloud2> vox;
+    vox.setInputCloud (cloud_read);
+    vox.setLeafSize (leaf_size_x,leaf_size_y,leaf_size_z);
+    vox.filter (*cloud_voxelized);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(*cloud_voxelized, *in_cloud);
+
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+    viewer->setBackgroundColor(0, 0, 0);
+
+    viewer->addPointCloud<pcl::PointXYZ>(
+        in_cloud,
+        "sample cloud");
+
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+    viewer->addCoordinateSystem(1.0);
+    viewer->initCameraParameters(); 
+
+    viewer->setRepresentationToWireframeForAllActors();
+
+    while (!viewer->wasStopped())
+    {
+        viewer->spinOnce(100);
+        std::this_thread::sleep_for(100ms);
+    }
+
+}
+
+void PointCloudFiltering::apply_voxel_filter_and_save(float leaf_size_x, float leaf_size_y, float leaf_size_z)
+{
+    pcl::PCLPointCloud2::Ptr cloud_read (new pcl::PCLPointCloud2 ());
+    pcl::PCLPointCloud2::Ptr cloud_voxelized (new pcl::PCLPointCloud2 ());
+    
+    //Read PCD files from the path given
+    pcl::PCDReader reader;
+    reader.read(in_folder_path_, *cloud_read);
 
     std::cerr << "PointCloud before filtering: " << cloud_read->width * cloud_read->height 
        << " data points (" << pcl::getFieldsList (*cloud_read) << ")." << std::endl;
@@ -82,9 +113,8 @@ void PCDFiltering::VoxelFilter(float leaf_size_x, float leaf_size_y, float leaf_
     std::cerr << "PointCloud after filtering: " << cloud_voxelized->width * cloud_voxelized->height 
        << " data points (" << pcl::getFieldsList (*cloud_voxelized) << ")." << std::endl;
 
-    std::string save_path = out_folder_path_ + object_name_ +"_voxelized.pcd";
     pcl::PCDWriter writer;
-    writer.write (save_path, *cloud_voxelized, Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+    writer.write (out_folder_path_, *cloud_voxelized, Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
 }
 
 } // namespace dataset_generation
--- ros_ws/src/dataset_generation/src/filter_point_clouds_node.cpp
+++ ros_ws/src/dataset_generation/src/filter_point_clouds_node.cpp
@@ -5,9 +5,10 @@
 int main()
 {
     std::string in_folder_pcd =
-        "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/";
+        "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/Jeep.pcd";
     std::string out_folder_pcd =
-        "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/";    
-    dataset_generation::PCDFiltering pcdf(in_folder_pcd,"Jeep",out_folder_pcd);
-    pcdf.VoxelFilter(0.05,0.05,0.05);
+        "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/Jeep_voxelized,pcd";    
+    dataset_generation::PointCloudFiltering pcf(in_folder_pcd,out_folder_pcd);
+    pcf.apply_voxel_filter_and_visualize(0.05,0.05,0.05);
+    pcf.apply_voxel_filter_and_save(0.05,0.05,0.05);
 }
