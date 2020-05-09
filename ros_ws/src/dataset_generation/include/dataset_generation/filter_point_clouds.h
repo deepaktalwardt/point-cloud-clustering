3d3277a6ae66cea6d1a9f64205e2737285bfae98
Deepak Talwar <deepaktalwardt@gmail.com>
2020-05-09 10:32:29 -0700

    Added Statistical Outlier removal

--- ros_ws/src/dataset_generation/include/dataset_generation/filter_point_clouds.h
+++ ros_ws/src/dataset_generation/include/dataset_generation/filter_point_clouds.h
@@ -11,6 +11,7 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
 #include <Eigen/Dense>
@@ -28,6 +29,7 @@ public:
 
     ~PointCloudFiltering();
 
+    // VoxelGrid Filtering for subsampling
     pcl::PCLPointCloud2 apply_voxel_filter(
         pcl::PCLPointCloud2::ConstPtr in_cloud,
         const float& leaf_size_x,
@@ -47,6 +49,7 @@ public:
         const float& leaf_size_y,
         const float& leaf_size_z);
     
+    // RadialFiltering for removing outliers
     pcl::PointCloud<pcl::PointXYZ>::Ptr apply_radial_filter(
         pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
         const float& radius,
@@ -63,6 +66,7 @@ public:
         const float& radius,
         const float& min_nb_neighbors);
 
+    // Ground Removal using PassThrough filter
     pcl::PointCloud<pcl::PointXYZ>::Ptr apply_ground_removal(
         pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
         const float& min_range,
@@ -78,6 +82,23 @@ public:
         std::string out_pcd_path,
         const float& min_range,
         const float& max_range);
+    
+    // Statistical Outlier removal
+    pcl::PointCloud<pcl::PointXYZ>::Ptr apply_statistical_outlier_removal(
+        pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
+        const int& mean_k_value,
+        const float& std_dev_mul_threshold);
+    
+    void apply_statistical_outlier_removal_and_visualize(
+        std::string in_pcd_path,
+        const int& mean_k_value,
+        const float& std_dev_mul_threshold);
+    
+    void apply_statistical_outlier_removal_and_save(
+        std::string in_pcd_path,
+        std::string out_pcd_path,
+        const int& mean_k_value,
+        const float& std_dev_mul_threshold);
 };
 
 /**
@@ -90,6 +111,9 @@ PointCloudFiltering::PointCloudFiltering() = default;
 */
 PointCloudFiltering::~PointCloudFiltering() = default;
 
+/***********************************************************************************************
+ * VoxelGrid Filtering
+ * ********************************************************************************************/
 /**
  * Applies the voxel filter to in_cloud and returns the filtered cloud
  * 
@@ -117,71 +141,6 @@ pcl::PCLPointCloud2 PointCloudFiltering::apply_voxel_filter(
        << " data points (" << pcl::getFieldsList(*cloud_voxelized) << ")." << std::endl;
     
     return *cloud_voxelized;
-}
-
-/**
- * Applies radial filter to the cloud pointer. Explanation for this is available here:
- * https://pcl-tutorials.readthedocs.io/en/master/radius_outlier_removal.html?highlight=radial%20filter
- * 
- * It will remove points from a PointCloud that do not have a given number of neighbors within a 
- * specific radius from their location.
-*/
-pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudFiltering::apply_radial_filter(
-    pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,
-    const float& radius,
-    const float& min_nb_neighbors)
-{
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
-
-    std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
-		   << " data points (" << pcl::getFieldsList (*cloud) << ")." << std::endl;
-
-	// Create the filtering object
-	pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
-
-	// build the filter
-	outrem.setInputCloud(cloud);
-	outrem.setRadiusSearch(radius);
-	outrem.setMinNeighborsInRadius(min_nb_neighbors);
-
-	// apply filter
-	outrem.filter (*cloud_filtered);
-
-	std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
-		   << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
-	
-    return cloud_filtered;
-}
-
-
-/**
- * Removes all the points on the ground plane from the input point cloud using the
- * PassThrough filter. Returns the filtered Point Cloud pointer.
- * Points that lie between min_range and max_range along the z-axis are KEPT. Everything
- * else is removed.
-*/
-pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudFiltering::apply_ground_removal(
-    pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
-    const float& min_range,
-    const float& max_range)
-{
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
-
-    std::cerr << "PointCloud before filtering: " << in_cloud->width * in_cloud->height 
-       << " data points (" << pcl::getFieldsList(*in_cloud) << ")." << std::endl;
-
-
-    // Create the filtering object
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud(in_cloud);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits (min_range, max_range);
-    pass.filter(*cloud_filtered);
-
-    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
-         << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
-        
-    return cloud_filtered;
 }
 
 
@@ -260,6 +219,44 @@ void PointCloudFiltering::apply_voxel_filter_and_save(
     writer.write(out_pcd_path, cloud_voxelized, Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
 }
 
+/***********************************************************************************************
+ * Radial Filtering for Outlier removal
+ * ********************************************************************************************/
+/**
+ * Applies radial filter to the cloud pointer. Explanation for this is available here:
+ * https://pcl-tutorials.readthedocs.io/en/master/radius_outlier_removal.html?highlight=radial%20filter
+ * 
+ * It will remove points from a PointCloud that do not have a given number of neighbors within a 
+ * specific radius from their location.
+*/
+pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudFiltering::apply_radial_filter(
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,
+    const float& radius,
+    const float& min_nb_neighbors)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
+
+    std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
+		   << " data points (" << pcl::getFieldsList (*cloud) << ")." << std::endl;
+
+	// Create the filtering object
+	pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
+
+	// build the filter
+	outrem.setInputCloud(cloud);
+	outrem.setRadiusSearch(radius);
+	outrem.setMinNeighborsInRadius(min_nb_neighbors);
+
+	// apply filter
+	outrem.filter (*cloud_filtered);
+
+	std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+		   << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
+	
+    return cloud_filtered;
+}
+
+
 /**
  * Applies radial filter to the point cloud at in_pcd_path and saves it at out_pcd_path
 */
@@ -319,6 +316,38 @@ void PointCloudFiltering::apply_radial_filter_and_visualize(
     }
 }
 
+/***********************************************************************************************
+ * Ground Removal using PassThrough Filtering
+ * ********************************************************************************************/
+/**
+ * Removes all the points on the ground plane from the input point cloud using the
+ * PassThrough filter. Returns the filtered Point Cloud pointer.
+ * Points that lie between min_range and max_range along the z-axis are KEPT. Everything
+ * else is removed.
+*/
+pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudFiltering::apply_ground_removal(
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
+    const float& min_range,
+    const float& max_range)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
+
+    std::cerr << "PointCloud before filtering: " << in_cloud->width * in_cloud->height 
+       << " data points (" << pcl::getFieldsList(*in_cloud) << ")." << std::endl;
+
+
+    // Create the filtering object
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(in_cloud);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits (min_range, max_range);
+    pass.filter(*cloud_filtered);
+
+    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
+         << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
+        
+    return cloud_filtered;
+}
 
 /**
  * Takes in_pcd_path to PCD file, removes ground and saves it into a PCD file at out_pcd_path
@@ -358,7 +387,7 @@ void PointCloudFiltering::apply_ground_removal_and_visualize(
     reader.read(in_pcd_path, *cloud);
     
     // Fill in the cloud data
-   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered = apply_ground_removal(
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered = apply_ground_removal(
         cloud,
         min_range,
         max_range);
@@ -379,6 +408,98 @@ void PointCloudFiltering::apply_ground_removal_and_visualize(
         viewer->spinOnce(100);
         std::this_thread::sleep_for(100ms);
     }
+}
+
+/***********************************************************************************************
+ * Statistical Outlier Removal
+ * ********************************************************************************************/
+/**
+ * Removes Outliers from the cloud using StatisticalOutlierRemoval returns the filtered cloud. 
+ * The number of neighbors to analyze for each point is set by `mean_k_value`. All points who have a 
+ * distance larger than the `std_dev_mul_threshold` * standard deviations of the mean distance 
+ * to the query point will be marked as outliers and removed.
+ * 
+ * Explanation available here: https://pcl-tutorials.readthedocs.io/en/master/statistical_outlier.html
+*/
+pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudFiltering::apply_statistical_outlier_removal(
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr in_cloud,
+    const int& mean_k_value,
+    const float& std_dev_mul_threshold)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
+
+    std::cerr << "PointCloud before filtering: " << in_cloud->width * in_cloud->height 
+       << " data points (" << pcl::getFieldsList(*in_cloud) << ")." << std::endl;
+
+    // Create the filtering object
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    sor.setInputCloud(cloud);
+    sor.setMeanK(mean_k_value);
+    sor.setStddevMulThresh(std_dev_mul_threshold);
+    sor.filter(*cloud_filtered);
+
+    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
+         << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
+        
+    return cloud_filtered;
+}
+
+/**
+ * Takes in_pcd_path to PCD file, removes outliers using StatisticalOutlierRemoval 
+ * and visualizes using PCLVisualizer. Use for debugging.
+*/
+void PointCloudFiltering::apply_statistical_outlier_removal_and_visualize(
+    std::string in_pcd_path,
+    const int& mean_k_value,
+    const float& std_dev_mul_threshold)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PCDReader reader;
+    reader.read(in_pcd_path, *cloud);
+    
+    // Fill in the cloud data
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered = apply_statistical_outlier_removal(
+        cloud,
+        mean_k_value,
+        std_dev_mul_threshold);
+
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+    viewer->setBackgroundColor(0, 0, 0);
+
+    viewer->addPointCloud<pcl::PointXYZ>(
+        cloud_filtered,
+        "Outliers Removed");
+
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Outliers Removed");
+    viewer->addCoordinateSystem(1.0);
+    viewer->initCameraParameters();
+
+    while(!viewer->wasStopped())
+    {
+        viewer->spinOnce(100);
+        std::this_thread::sleep_for(100ms);
+    }
+}
+
+void PointCloudFiltering::apply_statistical_outlier_removal_and_save(
+    std::string in_pcd_path,
+    std::string out_pcd_path,
+    const int& mean_k_value,
+    const float& std_dev_mul_threshold)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ> ());
+
+    // Fill in the cloud data
+    pcl::PCDReader reader;
+    reader.read(in_pcd_path, *cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered = apply_statistical_outlier_removal(
+        cloud,
+        mean_k_value,
+        std_dev_mul_threshold);
+
+    pcl::PCDWriter writer;
+    writer.write(out_pcd_path, *cloud_filtered);
 }
 
 } // namespace dataset_generation
