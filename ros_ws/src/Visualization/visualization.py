af0906177a224840bd07351b924f0e0b687368bc
hetavisaraiya <hetavisaraiya@gmail.com>
2020-05-11 12:37:49 -0700

    Correction in Histogram

--- ros_ws/src/Visualization/visualization.py
+++ ros_ws/src/Visualization/visualization.py
@@ -3,11 +3,14 @@ import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
 
+# getting files from directory and reading from files
 files = os.listdir('out_10_transformed')
 os.chdir('out_10_transformed')
 print(len(files))
+
 object_dictionary = dict()
 
+#Printing file name and size
 for file in files:
     print(file)
 
@@ -17,30 +20,25 @@ for file in files:
             print(line.strip().split(' ')[1])
             value = int(line.strip().split(' ')[1])
             key = file.split('-')[0]
+            # Adding key and value to object dictionary
             if key in object_dictionary:
                 object_dictionary[key].append(value)
             else:
                 object_dictionary[key] = [value]
             break
-
+#printing each object with points in each Cloud file
 print(object_dictionary)
 
-object_points = dict()
-
-for key in object_dictionary:
-    object_points[key] = Counter(object_dictionary[key])
-    object_points[key] = dict(object_points[key])
-
-print(object_points)
-
-for object in object_points.keys():
-    dataframe = pd.DataFrame.from_dict(object_points[object], orient='index')
+#Plotting Histogram for objects
+for object in object_dictionary.keys():
+    dataframe = pd.DataFrame(object_dictionary[object], columns=[object])
     print(dataframe)
-
-    ax = dataframe.plot.hist(bins=10, legend=False, title=object)
+    ax = dataframe.plot.hist(bins=100, legend=False, title=object)
+    plt.xlabel('Number of points')
+    plt.ylabel('Number of clouds')
     plt.show()
     fig = ax.get_figure()
-    fig.savefig(object+'.png')
+
 
 
 
