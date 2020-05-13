f1553a3a3b77fa9fd54e82d6caae2b6405617046
Deepak Talwar <deepaktalwardt@gmail.com>
2020-05-12 21:53:57 -0700

    Added more comments

--- ros_ws/src/dataset_generation/src/extract_point_cloud_objects_node.cpp
+++ ros_ws/src/dataset_generation/src/extract_point_cloud_objects_node.cpp
@@ -32,16 +32,6 @@ int main(int argc, char** argv)
      * Extract objects into individual PCDs. 
     */
 
-    // std::string in_folder_pcd =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/pcd";
-    
-    // std::string in_folder_dets3d =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/detections_3d";
-
-    // std::string out_folder_pcd =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/out_100_transformed";
-
-    // std::string bag_name = "bag6_2020-05-09-19-08-22.bag";
     std::string bag_name = "testbag1_2020-05-09-19-39-52.bag";
 
     std::string in_folder_pcd =
--- ros_ws/src/dataset_generation/src/filter_point_clouds_node.cpp
+++ ros_ws/src/dataset_generation/src/filter_point_clouds_node.cpp
@@ -3,71 +3,77 @@
 
 #include "dataset_generation/process_point_clouds.h"
 
+/**
+ * This node is used for processing/filtering point cloud objects.
+ * 
+ * All the code below is commented, please uncomment and appropriately change file paths to run this code.
+*/
+
 int main()
 {
-    // std::string out_pcd_file =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/Jeep_voxelized.pcd";
-
-    // pcf.apply_voxel_filter_and_save(in_pcd_file, out_pcd_file, 0.05, 0.05, 0.05);
-    // std::string in_pcd_file =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/Jeep.pcd";
-
-    // std::string out_pcd_file =
-    //     "/home/parshwa/Desktop/CMPE_255 Project/bag5_2020-05-05-15-11-17.bag-20200506T192755Z-001/bag5_2020-05-05-15-11-17.bag/Jeep_pass_through.pcd";
-
+    /******************************************************************************
+     * Uncomment to create PointCloudProcessing Object
+     * ***************************************************************************/
     // std::string in_pcd_file =
     //     "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/bag6_2020-05-09-19-08-22.bag/combined_objects_10/Jeep_combined.pcd";
     
     // std::string out_pcd_file =
-    //     "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/bag6_2020-05-09-19-08-22.bag/combined_objects_10/Sedan_combined_filtered.pcd";
-    
-    std::string out_pcd_file =
-        "/home/deepak/Downloads/Jeep_combined_filtered.pcd";
-
-    std::string out_vtk_file =
-        "/home/deepak/Downloads/Hatchback_mesh_combined_filtered.vtk";
+    //     "/home/deepak/Downloads/Jeep_combined_filtered.pcd";
 
     // std::string out_vtk_file =
-    //     "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/bag5_2020-05-05-15-11-17.bag/combined_objects_10_pcd/Hatchback_10_combined_filtered.vtk";
+    //     "/home/deepak/Downloads/Hatchback_mesh_combined_filtered.vtk";
 
-    dataset_generation::PointCloudProcessing pcp;
+    // dataset_generation::PointCloudProcessing pcp;
 
+    /**
+     * Uncomment to Apply Ground Removal
+    */
     // pcp.apply_ground_removal_and_visualize(in_pcd_file, 0.13, 2);
     // pcp.apply_ground_removal_and_save(in_pcd_file, out_pcd_file, 0.13, 2);
 
-    // // pcp.apply_statistical_outlier_removal_and_visualize(out_pcd_file, 5, 0.3);
+    /**
+     * Uncomment to Apply Statistical Outlier Removal
+    */
+    // pcp.apply_statistical_outlier_removal_and_visualize(out_pcd_file, 5, 0.3);
     // pcp.apply_statistical_outlier_removal_and_save(out_pcd_file, out_pcd_file, 5, 0.3);
 
+    /**
+     * Uncomment to apply Voxel Filtering
+    */
     // pcp.apply_voxel_filter_and_visualize(out_pcd_file, 0.1, 0.1, 0.1);
     // pcp.apply_voxel_filter_and_save(out_pcd_file, out_pcd_file, 0.1, 0.1, 0.1);
 
     // pcf.apply_voxel_filter_and_visualize(in_pcd_file, 0.05, 0.05, 0.05);
     // pcf.apply_voxel_filter_and_save(in_pcd_file, out_pcd_file, 0.05, 0.05, 0.05);
 
+    /**
+     * Uncomment to apply Radial Filtering
+    */
     // pcf.apply_radial_filter_and_visualize(in_pcd_file, 0.05, 3);
     // pcf.apply_radial_filter_and_save(in_pcd_file, out_pcd_file, 0.1, 7);
 
-    // pcp.apply_ground_removal_and_save(in_pcd_file, out_pcd_file, 0.1, 2);
-    // pcf.apply_pass_filter_and_save(in_pcd_file, out_pcd_file,0.15,2);
 
-    // pcp.apply_statistical_outlier_removal_and_visualize(in_pcd_file, 10, 0.5);
-
-    std::string in_pcd_file =
-        "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/testbag1_2020-05-09-19-39-52.bag/sources/sources_1/Hatchback_combined_filtered.pcd";
+    /********************************************************************
+     * Uncomment to save PCDs to Mesh
+     * *****************************************************************/
+    // std::string in_pcd_file =
+    //     "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/testbag1_2020-05-09-19-39-52.bag/sources/sources_1/Hatchback_combined_filtered.pcd";
 
     // std::string out_vtk_file =
     //     "/home/deepak/Dropbox/SJSU/Semesters/Spring2020/CMPE 255/Project/raw_msgs/testbag1_2020-05-09-19-39-52.bag/sources/sources_1/Hatchback_combined_mesh.vtk";
 
 
-    json mesh_options;
-    mesh_options["k_search"] = 100;
-    mesh_options["search_radius"] = 1.5;
-    mesh_options["mu"] = 10;
-    mesh_options["max_nearest_neighbors"] = 400;
-    mesh_options["max_surface_angle"] = M_PI/4;  // 45 degrees
-    mesh_options["min_surface_angle"] = M_PI/18; // 10 degrees
-    mesh_options["max_angle"] = 2 * M_PI/3;      // 120 degrees
-    mesh_options["normal_consistency"] = false;   
+    // json mesh_options;
+    // mesh_options["k_search"] = 100;
+    // mesh_options["search_radius"] = 1.5;
+    // mesh_options["mu"] = 10;
+    // mesh_options["max_nearest_neighbors"] = 400;
+    // mesh_options["max_surface_angle"] = M_PI/4;  // 45 degrees
+    // mesh_options["min_surface_angle"] = M_PI/18; // 10 degrees
+    // mesh_options["max_angle"] = 2 * M_PI/3;      // 120 degrees
+    // mesh_options["normal_consistency"] = false;   
 
-    pcp.convert_point_cloud_to_mesh(in_pcd_file, out_vtk_file, mesh_options);
+    // pcp.convert_point_cloud_to_mesh(in_pcd_file, out_vtk_file, mesh_options);
+
+    return 0;
 }
